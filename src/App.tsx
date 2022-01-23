8a2211761042d8c77e0494ca8a90f1fb1890c574
ymaged95 <youssef.maged@thndr.app>
2022-01-23 22:38:00 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import logo from "./logo2.png";
+import "./App.css";
+import Countdown from "react-countdown";
+
+let BD = new Date("2022-01-24");
+BD.setHours(BD.getHours() - 2);
+
+function App() {
+  return (
+    <div className="App">
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <img src={logo} className="App-logo" alt="logo" />
+        <Countdown date={BD} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
